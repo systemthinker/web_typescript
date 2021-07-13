e4c55452f9893626753d4b0971210e3163473ddb
systemthinker <robbertoutenaar@gmail.com>
2021-07-13 15:28:13 +0200

    added User class

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log('hi there!')
+import { User } from './models/User';
+
+const user = new User({ name: 'Robbert', age: 36 });
+
+console.log(user.get('name'));
+
+user.set({ name: 'Rob' });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
