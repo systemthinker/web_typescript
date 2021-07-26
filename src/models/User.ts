5b9b547d71bdba5126fb6410ee98f36321dcb5c9
systemthinker <robbertoutenaar@gmail.com>
2021-07-26 18:00:25 +0200

    added save method, for composition added Eventing class

--- src/models/User.ts
+++ src/models/User.ts
@@ -6,10 +6,14 @@ interface UserProps {
   age?: number;
 }
 
-type Callback = () => void;
+
 
 export class User {
-  events: { [key: string]: Callback[] } = {};
+
+  id =  this.get('id')
+  url = 'http://localhost:3000';
+
+  
 
   constructor(private data: UserProps) {}
 
@@ -21,29 +25,25 @@ export class User {
     Object.assign(this.data, update);
   }
 
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback) => {
-      callback();
-    });
-  }
+ 
+  
 
   fetch(): void {
     axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
+      .get(`${this.url}/users/${this.id}`)
       .then((res: AxiosResponse): void => {
         this.set(res.data);
       });
+  }
+
+  save(): void {
+    
+    if (this.get('id')){
+      
+      axios.put(`${this.url}/users/${this.id}`, this.data)
+    } else {
+      axios.post(`${this.url}/users`,this.data)
+      
+    }
   }
 }
