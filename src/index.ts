b9d1763794e14e5da27378709dbcf35e9bf80684
systemthinker <robbertoutenaar@gmail.com>
2021-08-02 07:08:02 +0200

    added set method to User class, fixed error where this was read incorrectly

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,12 @@ import { User } from './models/User';
 
 const user = new User({ name: 'Rob', age: 36 });
 
+// how this works on in js
+console.log(user.get('name'));
+
 user.on('change', () => {
   console.log('user was changed');
 });
+
+user.set({ name: 'Same Name' });
+console.log(user.get('name'));
